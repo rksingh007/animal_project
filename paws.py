514c0bb9265749271611d198c3f8060c8efc737e
rksingh007 <rajkunvarsingh711@gmail.com>
2021-07-02 11:15:44 +0530

    use form validation

--- paws.py
+++ paws.py
@@ -1,16 +1,28 @@
-"""Flask Application for Paws Rescue Center."""
-from flask import Flask,render_template
+from flask import Flask, render_template
+from forms import LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
+
+users = {
+    "archie.andrews@email.com": "football4life",
+    "veronica.lodge@email.com": "fashiondiva"
+}
+
 @app.route("/")
+def home():
+    return render_template("home.html")
 
-def homepage():
-    return render_template ("home.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.is_submitted():
+        print("Submitted")
+    if form.validate():
+        print("Valid")
+    if form.validate_on_submit():
+        print("Submitted and Valid")
+    return render_template("login.html", form = form)
 
-
-@app.route("/About page")
-def About_page():
-    return render_template("about.html")
-    
 if __name__ == "__main__":
-   
     app.run(debug=True)

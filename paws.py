5da8f8da27e225a69381b381cf25ae07b644f945
rksingh007 <rajkunvarsingh711@gmail.com>
2021-10-22 16:18:30 +0530

    add models

--- paws.py
+++ paws.py
@@ -7,13 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paws.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 """ model for pets"""
 class Pet(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String,unique = True)
+    name = db.Column(db.String, unique = True)
     age = db.Column(db.String)
     bio = db.Column(db.String)
     posted_by = db.Column(db.String,db.ForeignKey("user.id"))
@@ -52,26 +51,16 @@ finally:
     db.session.close()
 
 
-"""Information regarding the Pets in the System."""
-pets = [
-    {"id": 1, "name": "Nelly", "age": "5 weeks",
-     "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
-    {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
-    {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
-    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
-]
-
-"""Information regarding the Users in the System."""
-users = [
-    {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
-]
-
-
 @app.route("/")
 def homepage():
     pets = Pet.query.all()
+    print(pets)
+    us = User.query.all()
+    print(us)
+
     """View function for Home Page."""
     return render_template("home.html", pets=pets)
+
 
 
 @app.route("/about")
@@ -83,7 +72,7 @@ def about():
 @app.route("/details/<int:pet_id>")
 def pet_details(pet_id):
     """View function for Showing Details of Each Pet."""
-    Pet.query.get(pet_id)
+    pet = Pet.query.get(pet_id)
     if pet is None:
         abort(404, description="No Pet was Found with the given ID")
     return render_template("details.html", pet=pet)
